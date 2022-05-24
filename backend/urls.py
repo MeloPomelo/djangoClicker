bfc222ce87cf3abf95f26c11ae163066a45999b1
Daniil <volkov.daniil.vit@ya.ru>
2022-05-24 21:17:33 +0500

    add clicks and boost

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
 
+
+boosts = views.BoostViewSet.as_view({
+    'get': 'list',  # Получить список всех бустов
+    'post': 'create',  # Создать буст
+})
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.User_login.as_view(), name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('call_click/', views.call_click),
+    path('boosts/', boosts, name='boosts'),
 ]
